b637303f92945c6346d4820bfd59f97f614902b9
vince-sun <79555910+vince-sun@users.noreply.github.com>
2021-02-23 21:31:58 -0700

    Add name, edit addNumber(), edit reverseNumber()
    Added name to line 2
    Edited addNumber() to include previousNumber = previousNumber+num
    Added a new private variable, reverseNum.
    Edited reverseNumber() to include reverseNum= newNumber.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Vincent Sun
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,6 +14,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        previousNumber= previousNumber+num;
         System.out.print("Add num to previousNumber");
         return 0;
     }
@@ -45,7 +47,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum=newNumber;
         return newNumber;
     }
     
