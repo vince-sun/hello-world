001b1513ca476845ca858f03cf1cf359f3cb693a
vince-sun <79555910+vince-sun@users.noreply.github.com>
2021-02-23 21:40:21 -0700

    Swapping Code
    Swapped the code in findFactorial()  and reverseNumber().

--- GitDemo.java
+++ GitDemo.java
@@ -23,12 +23,11 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(double num) {
-        double factorialNumber = 0;
+     
+        double factorialNumber  = 1; 
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            factorialNumber  = factorialNumber  * i;
         }
         
         return factorialNumber;
@@ -40,11 +39,16 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(double num){
-        double newNumber = 1;
+        newNumber
+        double newNumber = 0;
         
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+        while( num != 0 ) {
+            double temp = num % 10;
+            newNumber = newNumber * 10 + temp;
+            num /= 10;
         }
+        
+
         
         return newNumber;
     }
